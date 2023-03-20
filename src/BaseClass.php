25bab7dc8b4ccdee247773c03dae828d4b2a9304
mfrouh <mohamedfrouh96@gmail.com>
2023-03-20 15:32:08 +0200

    edit

--- src/BaseClass.php
+++ src/BaseClass.php
@@ -7,23 +7,29 @@ use Exception;
 
 class BaseClass
 {
-    const Base_Url = 'https://4jawaly.net/api';
-    const Base_Url_New = 'https://api-sms.4jawaly.com/api/v1/account/area/sms/send';
+    const Base_Url = 'https://api-sms.4jawaly.com/api/v1/account/area';
 
     public function getBalance()
     {
-        try {
-            $data = [
-                'username' => config('sms4jawaly.username'),
-                'password' => config('sms4jawaly.password'),
-                'return' => 'json'
-            ];
+        $query = [];
+        $query["is_active"] = 1; // get active only
+        $query["order_by"] = "id"; // package_points, current_points, expire_at or id (default)
+        $query["order_by_type"] = "desc"; // desc or asc
+        $query["page"] = 1;
+        $query["page_size"] = 10;
+        $query["return_collection"] = 1; // if you want to get all collection
+        $response = Http::withBasicAuth(config('sms4jawaly.username'), config('sms4jawaly.password'))
+            ->withHeaders([
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ])
+            ->get(self::Base_Url . '/me/packages?' . http_build_query($query));
+        $response = (array)json_decode($response);
 
-            $response = Http::withOptions(['verify' => false])->get(self::Base_Url . '/getbalance.php', $data);
-            $response = json_decode($response);
-            return ['status' => true, 'response' => $response->currentuserpoints, 'message' => $response->MessageIs];
-        } catch (Exception $error) {
-            return ['status' => false, 'response' => $error, 'message' => 'error'];
+        if ($response['code'] == 200) {
+            return ['status' => true, 'response' => $response['total_balance'], 'message' => $response['message']];
+        } else {
+            return ['status' => false, 'response' => 'error', 'message' => $response['message']];
         }
     }
 
@@ -36,7 +42,7 @@ class BaseClass
         $messages["messages"][0]["sender"] = config('sms4jawaly.sender_name');
 
         $response = Http::withBasicAuth(config('sms4jawaly.username'), config('sms4jawaly.password'))
-            ->post(self::Base_Url_New, $messages);
+            ->post(self::Base_Url . '/sms/send', $messages);
 
         return json_decode($response);
     }
